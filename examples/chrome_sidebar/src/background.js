d54033c20c85e65f75634eb2fd0568881f60edf4
Usman Ahmad <work.usmanahmad@gmail.com>
2023-05-12 17:09:02 +0500

    sidebar_example_mv3 - chrome.browserAction, chrometabs and webRequest deprecated and fixed according to v3

--- examples/chrome_sidebar/src/background.js
+++ examples/chrome_sidebar/src/background.js
@@ -1,19 +1,19 @@
 import { attachHeadersListener } from 'chrome-sidebar'
 import { hosts, iframeHosts } from './settings'
 
-console.log('Chrome Github Trending Sidebar Extension Registered')
-
-chrome.browserAction.onClicked.addListener(tab => {
+chrome.action.onClicked.addListener(tab => {
   console.log('Browser Action Triggered')
-	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.id, {
-    file: 'entry.js'
-	})
-})
+	chrome.scripting.executeScript({
+    target: {
+      tabId: tab.id
+    },
+    files: ['entry.js'],
+  });
+});
 
 attachHeadersListener({
-  webRequest: chrome.webRequest,
+  declarativeNetRequest: chrome.declarativeNetRequest,
   hosts,
   iframeHosts,
   overrideFrameOptions: true
-})
+});
