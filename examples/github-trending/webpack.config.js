1349078ad0fa1b20663fc6c513797c97be909684
Teknuk <dev@teknuk.com>
2023-05-25 20:59:35 +0500

    dependencies_upgrade - webpack upgrade to 4.46.0 and babel related packages upgraded

--- examples/github-trending/webpack.config.js
+++ examples/github-trending/webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
+  mode: 'none',
   entry: {
     background: path.join(__dirname, './src/background'),
     entry: path.join(__dirname, './src/entry'),
@@ -27,10 +28,10 @@ module.exports = {
     extensions: ['.js']
   },
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js$/,
       loader: 'babel-loader',
-      query: {
+      options: {
         presets: ['react', 'es2015', 'stage-0']
       },
       exclude: /node_modules/
