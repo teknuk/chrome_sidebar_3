57ee22fb472da4b0e3864da4e9cd7d01901f48a6
Teknuk <dev@teknuk.com>
2023-05-26 15:52:15 +0500

    MV2 to MV3 - changed scripts to service_worker for background and browser_action to action in manifest.json

--- examples/github-trending/src/background.js
+++ examples/github-trending/src/background.js
@@ -3,7 +3,7 @@ import { hosts, iframeHosts } from './settings'
 
 console.log('Chrome Github Trending Sidebar Extension Registered')
 
-chrome.browserAction.onClicked.addListener(tab => {
+chrome.action.onClicked.addListener((tab) => {
   console.log('Browser Action Triggered')
 	// for the current tab, inject the "inject.js" file & execute it
 	chrome.tabs.executeScript(tab.id, {
