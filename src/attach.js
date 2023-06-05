f84a296293777a786d9ef7460265614e3f65e780
Usman Ahmad <ua@teknuk.com>
2023-06-05 21:33:26 +0500

    MV2 to MV3 - modified declarativeNetRequest.updateDynamicRules with unsafe-inline for Content-Security-Policy

--- src/attach.js
+++ src/attach.js
@@ -1,74 +1,51 @@
-
-export function attachHeadersListener({
-  declarativeNetRequest,
-  hosts,
-  iframeHosts,
-  overrideFrameOptions
-}) {
+export function attachHeadersListener({ hosts, iframeHosts, overrideFrameOptions }) {
   if (typeof hosts !== 'string') {
     if (hosts) {
-      hosts = hosts.join(' ')
+      hosts = hosts.join(' ');
     } else {
-      throw new Error('`hosts` option must be a string or array')
+      throw new Error('`hosts` option must be a string or array');
     }
   }
 
   if (typeof iframeHosts !== 'string') {
     if (iframeHosts) {
-      iframeHosts = iframeHosts.join(' ')
+      iframeHosts = iframeHosts.join(' ');
     } else {
-      throw new Error('`iframeHosts` option must be a string or array')
+      throw new Error('`iframeHosts` option must be a string or array');
     }
   }
 
-  const types  = ['main_frame']
+  const addRules = [
+    {
+      "id": 501,
+      "priority": 1,
+      "action": {
+        "type": "modifyHeaders",
+        "responseHeaders": [
+          {
+            "header": "Content-Security-Policy",
+            "operation": "append",
+            "value": `script-src 'self' 'unsafe-inline' ${hosts}; style-src 'self' 'unsafe-inline' ${hosts}; frame-src ${iframeHosts}; child-src ${hosts}`
+          }
+        ]
+      },
+      "condition": {
+        "urlFilter": "https://*/*, http://*/*",
+        "resourceTypes": ["main_frame"]
+      }
+    }
+  ];
 
   if (overrideFrameOptions) {
-    types.push('sub_frame')
+    addRules[0].action.responseHeaders.push({
+      "header": "X-Frame-Options",
+      "operation": "set",
+      "value": "ALLOWALL"
+    });
   }
-
-  declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: [513, 531],
-    addRules: [
-      {
-        id: 513, // Unique ID for the rule
-        priority: 1, // Rule priority
-        action: {
-          type: 'modifyHeaders',
-          responseHeaders: [
-            {
-              header: 'content-security-policy',
-              operation: 'set',
-              value: 'default-src \'self\'; script-src \'self\' ' + hosts + '; style-src \'self\' ' + hosts + '; frame-src \'self\' ' + iframeHosts + '; child-src \'self\' ' + hosts + ';',
-            },
-          ],
-        },
-        condition: {
-          urlFilter: hosts.split(' ').map(host => `*://${host}/*`).join(', '),
-          resourceTypes: ['main_frame'],
-        },
-      },
-      {
-        id: 531,
-        priority: 2,
-        action: {
-          type: 'modifyHeaders',
-          responseHeaders: [
-            {
-              header: 'content-security-policy',
-              operation: 'set',
-              value: 'default-src \'self\'; script-src \'self\' ' + hosts + '; style-src \'self\' ' + hosts + '; frame-src \'self\' ' + iframeHosts + '; child-src \'self\' ' + hosts + ';',
-            },
-          ],
-        },
-        condition: {
-          urlFilter: iframeHosts.split(' ').map(host => `*://${host}/*`).join(', '),
-          resourceTypes: ['sub_frame'],
-        },
-      },
-    ],
-  }, () => {
-    console.log('Rules updated successfully');
+  const removeRuleIds = [501]
+  chrome.declarativeNetRequest.updateDynamicRules({ addRules, removeRuleIds }, () => {
+    console.log("Rules updated.");
   });
 }
 
